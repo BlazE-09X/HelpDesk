b68606455dc5fb958d5aa1f7a64dfe7948d09495
BlazE-09X <aruzansaparbek172@gmail.com>
2026-03-14 12:12:05 +0500

    3 activity (User, Worker, Admin)

--- app/src/main/java/firstapp/helpdesk/Login.java
+++ app/src/main/java/firstapp/helpdesk/Login.java
@@ -1,6 +1,8 @@
 package firstapp.helpdesk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -11,17 +13,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
 
-    private EditText,
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login_activity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etEmail = findViewById(R.id.login);
+        etPassword = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.button);
+
+        btnLogin.setOnClickListener(v -> {
+            String email = etEmail.getText().toString();
+
+            //кто именно зашел в приложение
+            if (email.contains("admin")){
+                startActivity(new Intent(this, AdminMain.class));
+            }
+            else if (email.contains("worker")){
+                startActivity(new Intent(this, WorkerMain.class));
+            }
+            else {
+                startActivity(new Intent(this, UserMain.class));
+            }
         });
+
     }
 }
