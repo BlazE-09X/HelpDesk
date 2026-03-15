bd7e46393747d1dd16ccce9ad62307c3cd00da5e
BlazE-09X <aruzansaparbek172@gmail.com>
2026-03-15 20:28:24 +0500

    Add ticket_activity

--- app/src/main/java/firstapp/helpdesk/UserMain.java
+++ app/src/main/java/firstapp/helpdesk/UserMain.java
@@ -1,6 +1,8 @@
 package firstapp.helpdesk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -12,5 +14,14 @@ public class UserMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.usermain);
+
+        // Находим нашу кнопку с плюсом
+        View btnAdd = findViewById(R.id.btnAddTicket);
+
+        btnAdd.setOnClickListener(v -> {
+            // Создаем намерение (Intent) перейти с этого экрана на экран создания заявки
+            Intent intent = new Intent(UserMain.this, CreateTicketActivity.class);
+            startActivity(intent);
+        });
     }
 }
